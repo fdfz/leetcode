5d8c21083858780f439e479d441967a1082ae02e
fdfz <192144025+fdfz@users.noreply.github.com>
2024-12-19 23:24:49 +0800

    Update

--- 139 非最优.cc
+++ 139 非最优.cc
@@ -3,18 +3,20 @@
 #include <algorithm>
 #include<ranges>
 #include<iostream>
+#include <unordered_set>
 using namespace std;
 
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> set(wordDict.cbegin(),wordDict.cend());
         int size=s.size();
         vector<int> v(size+1);
         // v[i]表示不包括在内的i之前的在wordDict中
         v[0]=1;
         for(int i=0;i<=size;++i){
             for(int j=i;j>=0;j--){
-                if(v[j]==1&&ranges::contains(wordDict,s.substr(j,i-j))){
+                if(v[j]==1&&set.contains(s.substr(j,i-j))){
                     v[i]=1;
                     break;
                 }
